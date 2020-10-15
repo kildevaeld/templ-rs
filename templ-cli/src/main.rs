0da03a28608069465ae8aae9f1da7786017a48f8
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2020-10-15 20:55:40 +0200

    binary

--- templ-cli/src/main.rs
+++ templ-cli/src/main.rs
@@ -1,38 +1,52 @@
+use getopts::Options;
 use std::{env, fs};
 use templ_parser;
-use templ_runtime::{Number, Value};
+use templ_runtime::{Number, Runtime, Value};
 use templ_vm;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let o = env::args().skip(1).collect::<Vec<_>>();
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
 
-    if o.is_empty() {
-        eprintln!("usage templ-cli <path>");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut opts = Options::new();
+
+    opts.optflag("a", "ast", "Print ast")
+        .optflagopt("b", "bytecode", "print", "")
+        .optflagopt("t", "typed visitor", "", "")
+        .optflag("h", "help", "print usage");
+
+    let args = env::args().collect::<Vec<_>>();
+
+    if args.len() <= 1 {
+        print_usage(&args[0], opts);
+        return Ok(());
+    }
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
+    };
+    if matches.opt_present("h")
+    /*|| matches.free.is_empty()*/
+    {
+        print_usage(&args[0], opts);
         return Ok(());
     }
 
-    let templates = templ_vm::compiler::compile_path(&o[0]).expect("compile");
+    // let o = env::args().skip(1).collect::<Vec<_>>();
 
-    for t in templates {
-        println!("{}", t);
-        // let output = templ_vm::run_vm(
-        //     &t,
-        //     vec![
-        //         Value::String("Hello".into()),
-        //         Value::String("World".into()),
-        //         Value::Number(Number::Integer(101.0)),
-        //     ],
-        // );
+    // if o.is_empty() {
+    //     eprintln!("usage templ-cli <path>");
+    //     return Ok(());
+    // }
 
-        // println!("{}", output);
-    }
+    // let templates =
+    //     templ_vm::compiler::compile_path(&Runtime::new().build(), &o[0]).expect("compile");
 
-    // let data = fs::read_to_string(&o[0])?;
-    // let ast = templ_parser::parse(&data)?;
-
-    // let json = serde_json::to_string_pretty(&ast)?;
-
-    // println!("{}", json);
+    // for t in templates {
+    //     println!("{}", t);
+    // }
 
     Ok(())
 }
